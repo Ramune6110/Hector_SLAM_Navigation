af616f460104f9f4ca85e49bd050e5cc4d520d7b
Ramune6110 <arduino6110@gmail.com>
2021-05-03 11:57:01 +0900

    [modify]msg

--- scripts/person_detector.py
+++ scripts/person_detector.py
@@ -7,8 +7,6 @@ import threading
 
 # This import is for ROS integration
 import rospy
-# 自分で定義したmessageファイルから生成されたモジュール
-from Hector_SLAM_Navigation.msg import object_data
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int16
 from sensor_msgs.msg import Image,CameraInfo
@@ -33,18 +31,6 @@ class PersonDetector():
         self.HEIGHT = 50
 
         # Publish
-        """
-        self.pub_x_person     = rospy.Publisher('/x_person', Int16 , queue_size=10)
-        self.pub_x_centor     = rospy.Publisher('/x_centor', Int16 , queue_size=10)
-        self.pub_person_depth = rospy.Publisher('/person_depth', Int16 , queue_size=10)
-        """
-        # nodeの宣言 : publisherのインスタンスを作る
-        # input_dataというtopicにAdder型のmessageを送るPublisherをつくった
-        self.pub = rospy.Publisher('/object_data', object_data, queue_size=10)
- 
-        # object_data型のmessageのインスタンスを作る
-        self.msg = object_data()
-
         self.pub_cmd = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
         # Twist 型のデータ
@@ -89,24 +75,7 @@ class PersonDetector():
             ave = sum / ((self.WIDTH * 2) * (self.HEIGHT * 2))
             #rospy.loginfo('Class : person, Score: %.2f, Dist: %dmm ' %(self.person_bbox.probability, ave))
             print("%f [m]" % ave)
-            #print("%d" % x_person)
-            #print("%d" % y_person)
-
-            #print("%d" % w)
-            #print("%d" % h)
-
-            """
-            self.pub_x_person.publish(x_person)
-            self.pub_x_centor.publish(w / 2)
-            self.pub_person_depth.publish(ave)
-            """
-            self.msg.x_person = x_person
-            self.msg.person_distance = ave
-
-            # publishする関数
-            self.pub.publish(self.msg)
-            #print "published arg_x=%d arg_y=%d"%(self.msg.x_person, self.msg.person_distance)
-
+            
             velocity = 0
             rotation = 0
             error_angle = x_person - 320
@@ -114,30 +83,12 @@ class PersonDetector():
             controll_angle_gain = 0.006
             controll_distance_gain = 0.0004
 
-            """
-            if error_angle > 0:
-                velocity = 0.0
-                #rotation = -0.35
-                rotation = -controll_angle_gain * error_angle
-            elif error_angle < 0:
-                velocity = 0.0
-                #rotation = 0.35
-                rotation = -controll_angle_gain * error_angle
-            elif ave < 200.0:
-                velocity = 0.0
-                rotation = 0.0
-            """
-
             if error_angle > 0 and ave > 300:
-                #velocity = 0.0
-                #rotation = -0.35
                 velocity = controll_distance_gain * error_distance
                 if velocity > 0.2:
                     velocity = 0.2
                 rotation = -controll_angle_gain * error_angle
             elif error_angle < 0 and ave > 300:
-                #velocity = 0.0
-                #rotation = 0.35
                 velocity = controll_distance_gain * error_distance
                 if velocity > 0.2:
                     velocity = 0.2
@@ -203,9 +154,7 @@ if __name__ == '__main__':
         rospy.init_node('person_detector', anonymous=True)
         idc = PersonDetector()
         rospy.loginfo('idc Initialized')
-        #idc.start()
         rospy.spin()
-        #idc.finish()
 
     except rospy.ROSInterruptException:
         pass
